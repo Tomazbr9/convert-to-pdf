dfdbe140f9f6fe81c13e82448b6b5f89a5697945
Bruno Tomaz <tomazcraft@gmail.com>
2025-04-29 12:00:18 -0300

    A Função principal deve alguns scripts que foram para funções especificas para deixar o codigo mais limpo

--- app/services/file_service.py
+++ app/services/file_service.py
@@ -1,44 +1,56 @@
 from flask import current_app
 
-from converters.pdf_from_text import text_to_pdf
+from app.converters.pdf_from_text import text_to_pdf
 
 from werkzeug.utils import secure_filename
 
-from models.file import FileModel
+from app.models.file import FileModel
 
 import os
 
+from app import db
+
 import tempfile
 
-def save_converted_file(file, user_id=0):
-    original_name = os.path.splitext(file.filename)[0]
-    original_ext = os.path.splitext(file.filename)[1].lower()
-
+def get_output_path(original_name: str) -> str:
     media_path = os.path.join(current_app.root_path, 'media')
     os.makedirs(media_path, exist_ok=True)
     output_path = os.path.join(media_path, f'{original_name}.pdf')
+    return output_path
 
+def save_temp_file(file):
     temp_dir = tempfile.mkdtemp()
     temp_path = os.path.join(temp_dir, secure_filename(file.filename))
     file.save(temp_path)
+    return temp_path
+
+def save_file_in_database(output_path: str, user_id: int):
+    file_record = FileModel(
+        filename=os.path.basename(output_path),  # type: ignore 
+        filepath=output_path, # type: ignore
+        filesize=os.path.getsize(output_path), # type: ignore
+        user_id=user_id # type: ignore
+    )
+
+    db.session.add(file_record)
+    db.session.commit()
+
+    return file_record
+
+def save_converted_file(file, user_id=0) -> FileModel:
+
+    original_name = os.path.splitext(file.filename)[0]
+    original_ext = os.path.splitext(file.filename)[1].lower()
+
+    output_path = get_output_path(original_name)
+
+    temp_path = save_temp_file(file)
 
     if original_ext == '.txt':
         text_to_pdf(temp_path, output_path)
     else:
         raise ValueError('Tipo de arquivo não suportado para conversão')
     
-    file_record = FileModel(
-        filename=file.filename,  # type: ignore 
-        filepath=output_path, # type: ignore
-        filesize=os.path.getsize(output_path), # type: ignore
-        user_id=user_id # type: ignore
-    )
-    from app import db
-    db.session.add(file_record)
-    db.session.commit()
+    file_record = save_file_in_database(output_path, user_id)
 
     return file_record
-
-
-if __name__ == '__main__':
-    x = save_converted_file('ola.txt')
